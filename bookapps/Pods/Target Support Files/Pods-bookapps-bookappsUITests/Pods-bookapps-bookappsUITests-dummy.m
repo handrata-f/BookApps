4db775b1743d73540b5fd07ed2143cca1f8b28c8
Handrata <handrata.febrianto@horego.com>
2025-06-25 14:30:42 +0700

    initial init

---
+++ bookapps/Pods/Target Support Files/Pods-bookapps-bookappsUITests/Pods-bookapps-bookappsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_bookapps_bookappsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_bookapps_bookappsUITests
+@end
---
+++ bookapps/bookapps/bookappsApp.swift
@@ -0,0 +1,17 @@
+//
+//  bookappsApp.swift
+//  bookapps
+//
+//  Created by Handrata Febrianto on 25/06/25.
+//
+
+import SwiftUI
+
+@main
+struct bookappsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
